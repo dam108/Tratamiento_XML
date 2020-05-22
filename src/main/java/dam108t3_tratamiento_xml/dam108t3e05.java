25a18d739a403368785de7d7f5211f5122f4fa5a
Jose Doval <dovaldam108@gmail.com>
2020-05-22 02:37:40 +0200

    desde casa 22/05/2020 - Ejercicio 5

---
+++ src/main/java/dam108t3_tratamiento_xml/dam108t3e05.java
@@ -0,0 +1,34 @@
+/*
+Realiza un programa que muestre el título de todos los libros presentes en ‘librería.xml’ con su
+precio de aquellos libros que cuesten menos de 10 euros.
+ */
+package dam108t3_tratamiento_xml;
+import java.io.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
+public class dam108t3e05 {
+
+    public static void main(String[] args) {
+       File f = new File("archivos\\libreria.xml");
+       try (FileInputStream fis = new FileInputStream(f);
+            InputStreamReader isr = new InputStreamReader(fis, "UTF-8")) {
+           DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+           DocumentBuilder dB = factory.newDocumentBuilder();
+           Document doc = dB.parse(new InputSource(isr));
+           
+           NodeList listaLibros = doc.getElementsByTagName("libro");
+           for (int i = 0; i < listaLibros.getLength(); i++) {
+               Element elemento = (Element)listaLibros.item(i);
+               String titulo = elemento.getElementsByTagName("titulo").item(0).getTextContent();
+               String precio = elemento.getElementsByTagName("precio").item(0).getTextContent();
+               
+               if (Double.valueOf(precio) <= 10)
+               System.out.println("Titulo: "+titulo+", precio: "+precio);
+           }
+           
+       }
+       catch (Exception e){ e.printStackTrace();}
+    }
+    
+}
