9a881e3c4e92e571ab7ca237b00be3e5c390e0c6
Jose Doval <dovaldam108@gmail.com>
2020-05-22 02:17:52 +0200

    desde casa 22/05/2020 - Ejercicio 1

--- src/main/java/dam108t3_tratamiento_xml/dam108t3e01.java
+++ src/main/java/dam108t3_tratamiento_xml/dam108t3e01.java
@@ -1,10 +1,32 @@
-
+ /*
+Realiza un programa que muestre el nombre del documento raíz del archivo ‘librería.xml’
+*/
 package dam108t3_tratamiento_xml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 public class dam108t3e01 {
 
     public static void main(String[] args) {
-       // Primer Commit
+       File f = new File("archivos\\libreria.xml");
+       try (FileInputStream fis = new FileInputStream(f);
+            InputStreamReader isr = new InputStreamReader(fis, "UTF-8")) {
+           DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+           DocumentBuilder dB = factory.newDocumentBuilder();
+           Document doc = dB.parse(new InputSource(isr));
+           
+           System.out.println("Raiz: "+ doc.getDocumentElement().getNodeName());
+           
+       }
+       catch (Exception e){ e.printStackTrace();}
+    
+           
     }
     
 }
